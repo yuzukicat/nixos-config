50469cca4b9376a4a05fd6779bad0e1ff97f86b2
yuzuki.cat <yuzuki.cat@kamisu66.com>
2023-02-24 00:10:12 +0900

    remove luks && ssh key

--- nixos/blacksteel/configuration.nix
+++ nixos/blacksteel/configuration.nix
@@ -149,7 +149,7 @@
     users."oxa" = {
       isNormalUser = true;
       shell = pkgs.zsh;
-      passwordFile = config.sops.secrets.passwd.txt;
+      passwordFile = config.sops.secrets.passwd.path;
       uid = 1000;
       group = config.users.groups.oxa.name;
       extraGroups = [ "wheel" "kvm" "adbusers" "libvirtd" "wireshark" ];

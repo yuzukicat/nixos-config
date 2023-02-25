cefcec7435126387ffc9b507d4bbba76c587de86
yuzuki.cat <yuzuki.cat@kamisu66.com>
2023-02-25 21:07:59 +0900

    config home

--- nixos/blacksteel/configuration.nix
+++ nixos/blacksteel/configuration.nix
@@ -16,9 +16,9 @@
   # Install a proprietary or unfree package
   nixpkgs.config.allowUnfree = true;
 
-  nixpkgs.config.permittedInsecurePackages = with pkgs; [
-      "qtwebkit-5.212.0-alpha4"
-    ];
+  # nixpkgs.config.permittedInsecurePackages = with pkgs; [
+  #     "qtwebkit-5.212.0-alpha4"
+  #   ];
 
   nixpkgs.config.allowUnfreePredicate = drv:
     lib.elem (lib.getName drv) [

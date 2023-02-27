008033745b835bec131832c2b619815c268caa08
dawei.jiang <dawei.jiang@nowhere.co.jp>
2023-02-27 15:30:24 +0900

    opt console-env

--- nixos/modules/console-env.nix
+++ nixos/modules/console-env.nix
@@ -29,7 +29,6 @@
   };
   # Some defaults, override "basics.nix"
   programs.gnupg.agent.pinentryFlavor = lib.mkOverride 900 "qt";
-  programs.command-not-found.enable   = lib.mkOverride 900 true;
   services.flatpak.enable             = lib.mkOverride 900 true;
   environment.variables = let
     common = [

cd6995c39048a1a5ca4fb649ab4e412309ff61fa
yuzuki.cat <yuzuki.cat@kamisu66.com>
2023-02-28 00:00:16 +0900

    nix =

--- nixos/blacksteel/configuration.nix
+++ nixos/blacksteel/configuration.nix
@@ -269,16 +269,13 @@
   };
 
   nix = {
-    gc = {
-      automatic = true;
-      dates = "Wed,Sat 01:00";
-      options = "--delete-older-than 8d";
-    };
+
     settings = {
       package = inputs.nix-dram.packages.${config.nixpkgs.system}.nix-dram;
 
       default-flake = "flake:nixpkgs";
       environment = [ "SSH_AUTH_SOCK" ];
+    };
   };
   
   # environment.etc Refered from ../invar/configuration.nix

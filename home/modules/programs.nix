abb31ef8791aea539332cb1c7f4cef7c377ea382
dawei.jiang <dawei.jiang@nowhere.co.jp>
2023-03-02 12:45:57 +0900

    added vscode && extensions

--- home/modules/programs.nix
+++ home/modules/programs.nix
@@ -12,9 +12,64 @@ let
     pip
   ]);
 
-  myCodeOSS = with pkgs; [
-    (vscode-with-extensions.override {
-      vscodeExtensions = with vscode-extensions; [
+in {
+  home.packages = with pkgs; with libsForQt5; with plasma5; with kdeGear; with kdeFrameworks; [
+    # Console
+    runzip scc bubblewrap difftastic # Random stuff
+    xsel wl-clipboard # CLI-Desktop
+    beancount my.pkgs.double-entry-generator # Accounting
+    tealdeer man-pages # Manual
+    sops # Sops
+
+    # GUI
+    kolourpaint libreoffice mpv okular # Files
+    electrum electron-cash # Cryptocurrency
+    tdesktop # Messaging
+    wf-recorder obs-studio # Recording
+
+    # Dev
+    cachix patchelf nixpkgs-review nix-update nix-output-monitor # Nix utils
+    gcc ghc myPython myCodeOSS# Compiler & interpreters
+    gdb # Debugger
+    sqlite-interactive sqls# sqlite
+    cabal-install gnumake yarn binutils ruby_3_1 xclip
+    bash-completion cling elixir gh
+    go nodejs nodePackages.npm-check-updates
+
+    # Configuration from https://github.com/sauricat/flakes.git/home/home.nix
+    # system:
+    trash-cli bc
+    my.pkgs.hyfetch
+
+    # internet:
+    aria2 element-desktop vlc /*syncplay*/
+
+    # work:
+    scribus gwenview gimp krita calibre
+    xournalpp pdftag ocrmypdf poppler_utils
+    goldendict zotero
+
+    # non-oss:
+    zoom-us obsidian
+
+    autorandr discocss exa feh lieer mako ncmpcpp notmuch vscode
+  ];
+
+  programs.alacritty.settings.font.size = lib.mkForce 10;
+  programs.autorandr.enable = true;
+  programs.dircolors.enable = true;
+  programs.exa.enable = true;
+  programs.feh.enable = true;
+  programs.hyfetch.settings = {
+    preset = "transgender";
+    mode = "rgb";
+    color_align = {
+      mode = "horizontal";
+    };
+  };
+  programs.jq.enable = true;
+  programs.vscode = {
+    extensions = with pkgs.vscode-extensions; [
         ms-vscode.anycode
         editorconfig.editorconfig
         esbenp.prettier-vscode
@@ -65,71 +120,14 @@ let
         bierner.emojisense
         ms-vscode.hexeditor
         kddejong.vscode-cfn-lint
-      ];
-      # ++ vscode-utils.extensionsFromVscodeMarketplace [
-      #   {
-      #     name = "code-runner";
-      #     publisher = "formulahendry";
-      #     version = "0.6.33";
-      #     sha256 = "166ia73vrcl5c9hm4q1a73qdn56m0jc7flfsk5p5q41na9f10lb0";
-      #   }
-      # ];
-    })
-  ];
-in {
-  home.packages = with pkgs; with libsForQt5; with plasma5; with kdeGear; with kdeFrameworks; [
-    # Console
-    runzip scc bubblewrap difftastic # Random stuff
-    xsel wl-clipboard # CLI-Desktop
-    beancount my.pkgs.double-entry-generator # Accounting
-    tealdeer man-pages # Manual
-    sops # Sops
-
-    # GUI
-    kolourpaint libreoffice mpv okular # Files
-    electrum electron-cash # Cryptocurrency
-    tdesktop # Messaging
-    wf-recorder obs-studio # Recording
-
-    # Dev
-    cachix patchelf nixpkgs-review nix-update nix-output-monitor # Nix utils
-    gcc ghc myPython myCodeOSS# Compiler & interpreters
-    gdb # Debugger
-    sqlite-interactive sqls# sqlite
-    cabal-install gnumake yarn binutils ruby_3_1 xclip
-    bash-completion cling elixir gh
-    go nodejs nodePackages.npm-check-updates
-
-    # Configuration from https://github.com/sauricat/flakes.git/home/home.nix
-    # system:
-    trash-cli bc
-    my.pkgs.hyfetch
-
-    # internet:
-    aria2 element-desktop vlc /*syncplay*/
-
-    # work:
-    scribus gwenview gimp krita calibre
-    xournalpp pdftag ocrmypdf poppler_utils
-    goldendict zotero
-
-    # non-oss:
-    zoom-us obsidian
-
-    autorandr discocss exa feh lieer mako ncmpcpp notmuch
-  ];
-
-  programs.alacritty.settings.font.size = lib.mkForce 10;
-  programs.autorandr.enable = true;
-  programs.dircolors.enable = true;
-  programs.exa.enable = true;
-  programs.feh.enable = true;
-  programs.hyfetch.settings = {
-    preset = "transgender";
-    mode = "rgb";
-    color_align = {
-      mode = "horizontal";
-    };
+    ];
+    # ++ vscode-utils.extensionsFromVscodeMarketplace [
+    #   {
+    #     name = "code-runner";
+    #     publisher = "formulahendry";
+    #     version = "0.6.33";
+    #     sha256 = "166ia73vrcl5c9hm4q1a73qdn56m0jc7flfsk5p5q41na9f10lb0";
+    #   }
+    # ];
   };
-  programs.jq.enable = true;
 }

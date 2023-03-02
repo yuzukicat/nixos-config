8cd30d84fbadb7f8c21df8c2df9b0d590ebc4091
dawei.jiang <dawei.jiang@nowhere.co.jp>
2023-03-02 13:38:44 +0900

    rm unnecessary import

--- nixos/modules/console-env.nix
+++ nixos/modules/console-env.nix
@@ -20,8 +20,58 @@
     my.pkgs.nixos-rebuild-shortcut
     tmuxPlugins.nord
 
-    emacs wget home-manager zsh-nix-shell vscode.fhs
+    emacs wget home-manager zsh-nix-shell vscode
     htop iotop iftop
+    vscode-extensions.ms-vscode.anycode
+    vscode-extensions.editorconfig.editorconfig
+    vscode-extensions.esbenp.prettier-vscode
+    vscode-extensions.dbaeumer.vscode-eslint
+    vscode-extensions.donjayamanne.githistory
+    vscode-extensions.mhutchie.git-graph
+    vscode-extensions.codezombiech.gitignore
+    vscode-extensions.matklad.rust-analyzer
+    vscode-extensions.golang.go
+    vscode-extensions.zxh404.vscode-proto3
+    vscode-extensions.oderwat.indent-rainbow
+    vscode-extensions._2gua.rainbow-brackets
+    vscode-extensions.shardulm94.trailing-spaces
+    vscode-extensions.ms-python.python
+    vscode-extensions.ms-python.vscode-pylance
+    vscode-extensions.ms-pyright.pyright
+    vscode-extensions.ms-python.vscode-pylance
+    vscode-extensions.njpwerner.autodocstring
+    vscode-extensions.ms-toolsai.jupyter
+    vscode-extensions.ms-toolsai.jupyter-keymap
+    vscode-extensions.ms-toolsai.jupyter-renderers
+    vscode-extensions.mechatroner.rainbow-csv
+    vscode-extensions.graphql.vscode-graphql
+    vscode-extensions.irongeek.vscode-env
+    vscode-extensions.prisma.prisma
+    vscode-extensions.yzhang.markdown-all-in-one
+    vscode-extensions.bierner.markdown-checkbox
+    vscode-extensions.bierner.markdown-mermaid
+    vscode-extensions.bradlc.vscode-tailwindcss
+    vscode-extensions.angular.ng-template
+    vscode-extensions.kamikillerto.vscode-colorize
+    vscode-extensions.eg2.vscode-npm-script
+    vscode-extensions.wix.vscode-import-cost
+    vscode-extensions.msjsdiag.debugger-for-chrome
+    vscode-extensions.jnoortheen.nix-ide
+    vscode-extensions.kamadorueda.alejandra
+    vscode-extensions.bungcip.better-toml
+    vscode-extensions.ms-vscode.cmake-tools
+    vscode-extensions.timonwong.shellcheck
+    vscode-extensions.foxundermoon.shell-format
+    vscode-extensions.elixir-lsp.vscode-elixir-ls
+    vscode-extensions.gruntfuggly.todo-tree
+    vscode-extensions.pkief.material-icon-theme
+    vscode-extensions.catppuccin.catppuccin-vsc
+    vscode-extensions.bodil.file-browser
+    vscode-extensions.alexdima.copy-relative-path
+    vscode-extensions.rioj7.commandOnAllFiles
+    vscode-extensions.bierner.emojisense
+    vscode-extensions.ms-vscode.hexeditor
+    vscode-extensions.kddejong.vscode-cfn-lint
   ];
 
   programs.less = {

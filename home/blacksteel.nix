5db1f19e4c23ead5bf0bead11e103c43a6ae3b54
dawei.jiang <dawei.jiang@nowhere.co.jp>
2023-03-03 13:51:01 +0900

    added pm

--- home/blacksteel.nix
+++ home/blacksteel.nix
@@ -28,35 +28,35 @@
     inputs.plasma-manager.homeManagerModules.plasma-manager
   ];
 
-  programs.plasma = {
-    enable = true;
+  # programs.plasma = {
+  #   enable = true;
 
-    # Some high-level settings:
-    workspace.clickItemTo = "select";
+  #   # Some high-level settings:
+  #   workspace.clickItemTo = "select";
 
-    hotkeys.commands."Launch Konsole" = {
-      key = "Meta+Alt+K";
-      command = "konsole";
-    };
+  #   hotkeys.commands."Launch Konsole" = {
+  #     key = "Meta+Alt+K";
+  #     command = "konsole";
+  #   };
 
-    # Some mid-level settings:
-    shortcuts = {
-      ksmserver = {
-        "Lock Session" = [ "Screensaver" "Meta+Ctrl+Alt+L" ];
-      };
+  #   # Some mid-level settings:
+  #   shortcuts = {
+  #     ksmserver = {
+  #       "Lock Session" = [ "Screensaver" "Meta+Ctrl+Alt+L" ];
+  #     };
 
-      kwin = {
-        "Expose" = "Meta+,";
-        "Switch Window Down" = "Meta+J";
-        "Switch Window Left" = "Meta+H";
-        "Switch Window Right" = "Meta+L";
-        "Switch Window Up" = "Meta+K";
-      };
-    };
+  #     kwin = {
+  #       "Expose" = "Meta+,";
+  #       "Switch Window Down" = "Meta+J";
+  #       "Switch Window Left" = "Meta+H";
+  #       "Switch Window Right" = "Meta+L";
+  #       "Switch Window Up" = "Meta+K";
+  #     };
+  #   };
 
-    # A low-level setting:
-    files."baloofilerc"."Basic Settings"."Indexing-Enabled" = false;
-  };
+  #   # A low-level setting:
+  #   files."baloofilerc"."Basic Settings"."Indexing-Enabled" = false;
+  # };
 
   programs.zsh.loginExtra = ''
     if [[ -z $DISPLAY && "$(tty)" = /dev/tty1 ]] && type sway >/dev/null; then

ceb400ac329353e5945545e303d5fd4511187e88
dawei.jiang <dawei.jiang@nowhere.co.jp>
2023-03-03 13:50:20 +0900

    added pm

--- home/blacksteel.nix
+++ home/blacksteel.nix
@@ -28,6 +28,36 @@
     inputs.plasma-manager.homeManagerModules.plasma-manager
   ];
 
+  programs.plasma = {
+    enable = true;
+
+    # Some high-level settings:
+    workspace.clickItemTo = "select";
+
+    hotkeys.commands."Launch Konsole" = {
+      key = "Meta+Alt+K";
+      command = "konsole";
+    };
+
+    # Some mid-level settings:
+    shortcuts = {
+      ksmserver = {
+        "Lock Session" = [ "Screensaver" "Meta+Ctrl+Alt+L" ];
+      };
+
+      kwin = {
+        "Expose" = "Meta+,";
+        "Switch Window Down" = "Meta+J";
+        "Switch Window Left" = "Meta+H";
+        "Switch Window Right" = "Meta+L";
+        "Switch Window Up" = "Meta+K";
+      };
+    };
+
+    # A low-level setting:
+    files."baloofilerc"."Basic Settings"."Indexing-Enabled" = false;
+  };
+
   programs.zsh.loginExtra = ''
     if [[ -z $DISPLAY && "$(tty)" = /dev/tty1 ]] && type sway >/dev/null; then
       exec systemd-cat --identifier=sway sway

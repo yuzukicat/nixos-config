b997a293e151aeaf9ed1352f6c4de4552aab3f2d
yuzuki.cat <yuzuki.cat@kamisu66.com>
2023-03-06 22:23:34 +0900

    Added lsp servers

--- home/modules/programs.nix
+++ home/modules/programs.nix
@@ -83,27 +83,27 @@ in {
                                   (builtins.readDir dir));
         in readRecursively ./emacs;
     extraPackages = with inputs.emacs-overlay.packages.${pkgs.system}.emacsGit; [
-      use-package
-      ligature
-      diminish
-      winum
-      ivy
-      counsel
-      swiper
-      flycheck
-      doom-themes
-      nix-mode
-      markdown-mode
-      yaml-mode
-      rust-mode
-      elixir-mode
-      cargo
-      dired-single
-      dirvish
-      pdf-tools
-      tree-sitter-langs
-      undo-tree
-      cmake-mode
+      epkgs.use-package
+      epkgs.ligature
+      epkgs.diminish
+      epkgs.winum
+      epkgs.ivy
+      epkgs.counsel
+      epkgs.swiper
+      epkgs.flycheck
+      epkgs.doom-themes
+      epkgs.nix-mode
+      epkgs.markdown-mode
+      epkgs.yaml-mode
+      epkgs.rust-mode
+      epkgs.elixir-mode
+      epkgs.cargo
+      epkgs.dired-single
+      epkgs.dirvish
+      epkgs.pdf-tools
+      epkgs.tree-sitter-langs
+      epkgs.undo-tree
+      epkgs.cmake-mode
      ];
     overrides = self: super: rec {
       tree-sitter-langs = self.tree-sitter-langs.withPlugins

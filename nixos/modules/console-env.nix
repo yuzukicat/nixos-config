ebbd8fd68e2d642f8a316ffc12afb14a7406a287
yuzuki.cat <yuzuki.cat@kamisu66.com>
2023-03-06 23:01:11 +0900

    Added lsp servers

--- nixos/modules/console-env.nix
+++ nixos/modules/console-env.nix
@@ -52,7 +52,7 @@
       defaultInitFile = true;
 
       # Package is optional, defaults to pkgs.emacs
-      package = inputs.emacs-overlay.packages.${pkgs.system}.emacsGit;
+      package = pkgs.emacsUnstable;
 
       # By default emacsWithPackagesFromUsePackage will only pull in
       # packages with `:ensure`, `:ensure t` or `:ensure <package name>`.
@@ -156,7 +156,7 @@
   # programs.gnupg.agent.pinentryFlavor = "qt";
   services.flatpak.enable = true; #  A framework for distributing desktop applications across various Linux distributions.
   services.emacs = {
-    package = inputs.emacs-overlay.packages.${pkgs.system}.emacsGit;
+    package = pkgs.emacsUnstable;
     enable = true;
   };
   # To use VS Code under Wayland, set the environment variable NIXOS_OZONE_WL=1:

60245f945e240bf6fc2b8fbc9e01d2a599e52d7b
dawei.jiang <dawei.jiang@nowhere.co.jp>
2023-03-06 11:22:18 +0900

    emcas

--- home/modules/emacs/default.nix
+++ home/modules/emacs/default.nix
@@ -1,16 +1,22 @@
 { config, lib, pkgs, inputs, my, ... }:
 
-{
+let
+  home = config.home.homeDirectory;
+in {
+  home.packages = with pkgs; with inputs.emacs-overlay.packages.${pkgs.system}.emacsGit; [
+    all-the-icons
+  ];
+
   programs.emacs = {
     enable = true;
     package = inputs.emacs-overlay.packages.${pkgs.system}.emacsGit;
     extraConfig = ''
       (setq standard-indent 2)
     '';
-    extraPackages = epkgs: [ epkgs.emms epkgs.magit epkgs.use-package epkgs.all-the-icons ];
+    extraPackages = epkgs: [ epkgs.emms epkgs.magit epkgs.use-package ];
     overrides = self: super: rec {
       haskell-mode = self.melpaPackages.haskell-mode;
-      lambda-line = epkgs: [ my.pkgs.lambda-line.nix ];
+      lambda-line = my.pkgs.lambda-line.nix;
     };
   };
 }

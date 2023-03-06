c1dd0d501cb066a189edba50189922092b57143c
dawei.jiang <dawei.jiang@nowhere.co.jp>
2023-03-06 13:50:17 +0900

    emcas

--- home/modules/emacs.nix
+++ home/modules/emacs.nix
@@ -15,13 +15,26 @@ in {
       (setq standard-indent 2)
     '';
     extraPackages = [];
-    overrides = self: super: rec {
-      toggle-one-window = self.trivialBuild rec {
-        pname = "toggle-one-window";
-        ename = pname;
-        version = "git";
-        src = inputs.epkgs-toggle-one-window;
+    overrides = epkgs: epkgs // (let
+        vterm-mouse-support = epkgs.melpaPackages.vterm.overrideAttrs (old: {
+          patches = (old.patches or [ ])
+                    ++ [ ./patch/vterm-mouse-support.patch ];
+        });
+      in {
+        tree-sitter-langs = epkgs.tree-sitter-langs.withPlugins
+          # Install all tree sitter grammars available from nixpkgs
+          (grammars: builtins.filter lib.isDerivation (lib.attrValues (grammars // {
+            tree-sitter-nix = grammars.tree-sitter-nix.overrideAttrs (old: {
+              version = "fixed";
+              src = inputs.tree-sitter-nix-oxa;
+            });
+          })));
+        toggle-one-window = epkgs.trivialBuild rec {
+          pname = "toggle-one-window";
+          ename = pname;
+          version = "git";
+          src = inputs.epkgs-toggle-one-window;
+        };
       };
-    };
   };
 }

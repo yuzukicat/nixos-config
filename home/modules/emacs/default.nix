ca56006b471244376698c249bffe73281cbf46f9
dawei.jiang <dawei.jiang@nowhere.co.jp>
2023-03-06 11:37:17 +0900

    emcas

--- home/modules/emacs/default.nix
+++ home/modules/emacs/default.nix
@@ -1,38 +1,21 @@
 { config, lib, pkgs, inputs, my, ... }:
 
-let
-  home = config.home.homeDirectory;
-in {
-  home.packages = with pkgs; with inputs.emacs-overlay.packages.${pkgs.system}; [
-    (emacsWithPackagesFromUsePackage {
-      package = emacsGit;  # replace with pkgs.emacsPgtk, or another version if desired.
-      # config = path/to/your/config.el;
-      # config = path/to/your/config.org; # Org-Babel configs also supported
-
-      # Optionally provide extra packages not in the configuration file.
-      extraEmacsPackages = epkgs: [
-        epkgs.use-package;
-      ];
-
-      # Optionally override derivations.
-      override = epkgs: epkgs // {
-        # somePackage = epkgs.melpaPackages.somePackage.overrideAttrs(old: {
-        #    # Apply fixes here
-        # });
-        lambda-line = my.pkgs.lambda-line.nix;
-      };
-    })
-  ];
-
+{
   programs.emacs = {
     enable = true;
-    package = home.packages.emacsGit;
+    package = inputs.emacs-overlay.packages.${pkgs.system}.emacsGit;
     extraConfig = ''
       (setq standard-indent 2)
     '';
-    extraPackages = epkgs: [ epkgs.emms epkgs.magit epkgs.use-package ];
+    extraPackages = with emacsPackages; [
+      emms
+      magit
+      use-package
+      all-the-icons
+    ];
     overrides = self: super: rec {
       haskell-mode = self.melpaPackages.haskell-mode;
+      lambda-line = my.pkgs.lambda-line.nix;
     };
   };
 }

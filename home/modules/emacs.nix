56ccc157ec76c0c28b003fcda4930457862c0a72
dawei.jiang <dawei.jiang@nowhere.co.jp>
2023-03-06 13:32:17 +0900

    emcas

--- home/modules/emacs.nix
+++ home/modules/emacs.nix
@@ -6,8 +6,17 @@ let
 
 in {
   home.packages = with pkgs; with inputs.emacs-overlay.packages.${pkgs.system}; [
-    (emacs.override {
-      package = emacsGit;
-    })
+
   ];
+  programs.emacs = {
+    enable = true;
+    package = inputs.emacs-overlay.packages.${pkgs.system}.emacsGit;
+    extraConfig = ''
+      (setq standard-indent 2)
+    '';
+    extraPackages = [];
+    overrides = {
+      lambda-line = my.pkgs.lambda-line;
+    };
+  };
 }

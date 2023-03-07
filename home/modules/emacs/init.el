938ec5833050339d35bf605d8c2852033bb7540d
dawei.jiang <dawei.jiang@nowhere.co.jp>
2023-03-07 14:37:07 +0900

    added some init config for emacs

--- home/modules/emacs/init.el
+++ home/modules/emacs/init.el
@@ -27,6 +27,15 @@
 (require 'shu-c)
 (require 'shu-tex)
 
+(setq confirm-kill-emacs #'yes-or-no-p)      ; 在关闭 Emacs 前询问是否确认关闭，防止误触
+(electric-pair-mode t)                       ; 自动补全括号
+(add-hook 'prog-mode-hook #'show-paren-mode) ; 编程模式下，光标在括号上时高亮另一个括号
+(column-number-mode t)                       ; 在 Mode line 上显示列号
+(global-auto-revert-mode t)                  ; 当另一程序修改了文件时，让 Emacs 及时刷新 Buffer
+(setq make-backup-files nil)                 ; 关闭文件自动备份
+(add-hook 'prog-mode-hook #'hs-minor-mode)   ; 编程模式下，可以折叠代码块
+(global-display-line-numbers-mode 1)         ; 在 Window 显示行号
+(setq display-line-numbers-type 'relative)   ; （可选）显示相对行号
 
 (global-set-key (kbd "C-z") 'undo)
 (global-unset-key (kbd "C-x C-z"))

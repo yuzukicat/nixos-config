e8468fd483761482e1b6b6588101b3504c150cc0
dawei.jiang <dawei.jiang@nowhere.co.jp>
2023-03-09 18:25:31 +0900

    ISO extraModules added

--- flake.nix
+++ flake.nix
@@ -288,7 +288,7 @@
       };
 
       minimal-image = mkSystem "minimal-image" "x86_64-linux" inputs.nixpkgs {
-        extraModules = with nixosModules; [ home-manager sops plsama-5-27 ];
+        extraModules = with nixosModules; [ home-manager sops plsama-5-27 emacs-overlay ];
       };
     };
 

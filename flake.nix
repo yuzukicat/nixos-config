091a925ef2c7fabd100b6aa77ecac6b405c40108
yuzuki.cat <yuzuki.cat@kamisu66.com>
2023-03-11 19:45:58 +0900

    Update ISO

--- flake.nix
+++ flake.nix
@@ -288,7 +288,7 @@
       };
 
       minimal-image = mkSystem "minimal-image" "x86_64-linux" inputs.nixpkgs {
-        extraModules = with nixosModules; [ home-manager plsama-5-27 emacs-overlay ];
+        extraModules = with nixosModules; [ sops plsama-5-27 emacs-overlay ];
       };
     };
 
--- home/minimal-iso.nix
+++ home/minimal-iso.nix
@@ -5,23 +5,21 @@
 
   imports = [
     ./modules/wayland-dpi.nix
-    ./modules/alacritty.nix
+    # ./modules/alacritty.nix
     ./modules/direnv.nix
-    ./modules/firefox.nix
+    # ./modules/firefox.nix
     ./modules/git.nix
     ./modules/gpg.nix
     ./modules/lf.nix
-    ./modules/mail.nix
+    # ./modules/mail.nix
     ./modules/mapping.nix
-    ./modules/mime-apps.nix
-    ./modules/programs.nix
-    # ./modules/rime-fcitx.nix
-    ./modules/rust.nix
-    ./modules/task.nix
-    ./modules/tmux.nix
+    # ./modules/mime-apps.nix
+    # ./modules/programs.nix
+    # ./modules/rust.nix
+    # ./modules/task.nix
+    # ./modules/tmux.nix
     ./modules/user-dirs.nix
-    # ./modules/xdgify.nix
-    ./modules/helix
+    # ./modules/helix
     ./modules/shell
   ];
 

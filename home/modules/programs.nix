8370e61f1d8f18cc6037fc5a94cecac19fdf77cc
yuzuki.cat <yuzuki.cat@kamisu66.com>
2023-03-12 21:19:42 +0900

    Update Input Method

--- home/modules/programs.nix
+++ home/modules/programs.nix
@@ -74,7 +74,7 @@ in {
     goldendict zotero
 
     # non-oss:
-    zoom-us obsidian my.pkgs.librime-lua wordninja
+    zoom-us obsidian my.pkgs.librime-lua
     docker
     discocss #?discord with bug
     notmuch # email engine

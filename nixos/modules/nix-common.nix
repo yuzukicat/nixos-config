7da4d3116bd094ae1cf807fa0a3354fc7b974bed
yuzukicat <yuzuki.cat@kamisu66.com>
2023-07-31 09:35:31 +0900

    rm nix dram

--- nixos/modules/nix-common.nix
+++ nixos/modules/nix-common.nix
@@ -11,7 +11,7 @@
   #   ];
 
   nix = {
-    package = inputs.nix-dram.packages.${config.nixpkgs.system}.nix-dram;
+    # package = inputs.nix-dram.packages.${config.nixpkgs.system}.nix-dram;
 
     gc = {
       automatic = true;
@@ -20,8 +20,8 @@
     };
 
     settings = {
-      default-flake = "flake:nixpkgs";
-      environment = ["SSH_AUTH_SOCK"];
+      # default-flake = "flake:nixpkgs";
+      # environment = ["SSH_AUTH_SOCK"];
       experimental-features = [
         "nix-command"
         "flakes"

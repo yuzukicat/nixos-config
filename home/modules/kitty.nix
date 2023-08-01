e6e751a89d0ad6dabca4687cff4bf284a69dce2e
yuzukicat <yuzuki.cat@kamisu66.com>
2023-08-01 14:57:49 +0900

    kitty opacity

--- home/modules/kitty.nix
+++ home/modules/kitty.nix
@@ -14,7 +14,7 @@
       scrollback_lines = 10000;
       enable_audio_bell = false;
       update_check_interval = 0;
-      background_opacity = "0.35";
+      background_opacity = "0.95";
       confirm_os_window_close = "0";
     };
 

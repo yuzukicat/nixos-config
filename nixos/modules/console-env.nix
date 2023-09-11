71e996771cabe87a8383d236d4c11e8d3de362da
yuzukicat <yuzuki.cat@kamisu66.com>
2023-09-11 09:09:43 +0900

    replace exa with eza

--- nixos/modules/console-env.nix
+++ nixos/modules/console-env.nix
@@ -26,7 +26,7 @@
     git
     strace
     pv # Monitoring of data being sent through pipe, including copying file.
-    exa # exa --long --header --recurse
+    eza # exa --long --header --recurse
     fd
     ripgrep
     lsof

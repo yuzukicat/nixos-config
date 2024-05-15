e9b379efdc814d4e4a4ba1a3688fd76a6122a45c
yuzukicat <yuzuki.cat@kamisu66.com>
2024-05-15 17:10:41 +0900

    flake update for blacksteel

--- home/modules/kitty.nix
+++ home/modules/kitty.nix
@@ -7,7 +7,8 @@
     font = {
       package = pkgs.fira-code;
       name = "fira-code";
-      size = 30;
+      size = 16;
+      # size = 30;
       # size = 12 * config.wayland.dpi / 96;
     };
     settings = {

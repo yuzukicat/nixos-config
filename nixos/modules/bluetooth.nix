a5ce4456e1936efde72e1e569b548bd32189fb49
yuzukicat <yuzuki.cat@kamisu66.com>
2024-06-12 06:49:23 +0900

    test bluetooth A2DP profile

--- nixos/modules/bluetooth.nix
+++ nixos/modules/bluetooth.nix
@@ -1,15 +1,19 @@
-{ ... }:
-
-{
+{...}: {
   # Bluetooth
   hardware.bluetooth.enable = true;
+  hardware.bluetooth.settings = {
+    General = {
+      Enable = "Source,Sink,Media,Socket";
+    };
+  };
   # services.blueman.enable = true;
   # services.dbus.packages = with pkgs; [
   #   bluez
   #   blueman
   # ];
-  systemd.tmpfiles.rules = [ # See https://github.com/NixOS/nixpkgs/issues/170573
+  systemd.tmpfiles.rules = [
+    # See https://github.com/NixOS/nixpkgs/issues/170573
     "d /var/lib/bluetooth 700 root root - -"
   ];
-  systemd.targets."bluetooth".after = [ "systemd-tmpfiles-setup.service" ];
+  systemd.targets."bluetooth".after = ["systemd-tmpfiles-setup.service"];
 }
